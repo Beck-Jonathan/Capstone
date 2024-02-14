7b0d5d460ea02805635368094700891767d13704
Nathan Toothaker <nathan-toothaker@student.kirkwood.edu>
2024-02-13 19:12:33 -0600

    added Consumer View shell

---
+++ NightRiderWPF/ConsumerMain.xaml.cs
@@ -0,0 +1,106 @@
+﻿using NightRiderWPF.DeveloperView;
+using NightRiderWPF.Resources;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NightRiderWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class ConsumerMain : Window
+    {
+        public ConsumerMain()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is ListBox listBox && listBox.SelectedItem is ListBoxItem item)
+            {
+                switch (item.Name)
+                {
+                    case "SamplePage":
+                        PageViewer.Navigate(new SamplePage());
+                        break;
+                        // Your cases here
+                }
+            }
+        }
+
+        private void btnClients_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn && btn.Name == "btnClients")
+            {
+                foreach (var child in stackMainNav.Children)
+                {
+                    if (child is Button button)
+                    {
+                        button.Background = Statics.SecondaryColor;
+                    }
+                }
+                btn.Background = Statics.PrimaryColor;
+            }
+        }
+
+        private void btnEmployees_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn && btn.Name == "btnEmployees")
+            {
+                foreach (var child in stackMainNav.Children)
+                {
+                    if (child is Button button)
+                    {
+                        button.Background = Statics.SecondaryColor;
+                    }
+                }
+                btn.Background = Statics.PrimaryColor;
+            }
+        }
+
+        private void btnVehicles_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn && btn.Name == "btnVehicles")
+            {
+                foreach (var child in stackMainNav.Children)
+                {
+                    if (child is Button button)
+                    {
+                        button.Background = Statics.SecondaryColor;
+                    }
+                }
+                btn.Background = Statics.PrimaryColor;
+            }
+        }
+
+        private void btnMaintenance_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn && btn.Name == "btnMaintenance")
+            {
+                foreach (var child in stackMainNav.Children)
+                {
+                    if (child is Button button)
+                    {
+                        button.Background = Statics.SecondaryColor;
+                    }
+                }
+                btn.Background = Statics.PrimaryColor;
+            }
+        }
+    }
+}
+// checked by James Williams
---
+++ NightRiderWPF/Resources/Statics.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace NightRiderWPF.Resources
+{
+    public class Statics
+    {
+        // Main colors
+        public static SolidColorBrush BackgroundMain => new SolidColorBrush(Colors.AliceBlue);
+        public static SolidColorBrush PrimaryColor => new SolidColorBrush((Color)ColorConverter.ConvertFromString("#7081A3"));
+        public static SolidColorBrush SecondaryColor => new SolidColorBrush((Color)ColorConverter.ConvertFromString("#3D527A"));
+        public static SolidColorBrush AccentColor => new SolidColorBrush((Color)ColorConverter.ConvertFromString("#20355C"));
+        public static SolidColorBrush ButtonText => new SolidColorBrush((Color)ColorConverter.ConvertFromString("#CFD5E2"));
+
+        // Images
+        public static BitmapImage LogoWide => new BitmapImage(new Uri("pack://application:,,,/NightRiderWPF;component/Resources/Images/NightRider-01.png"));
+        public static BitmapImage Logo => new BitmapImage(new Uri("pack://application:,,,/NightRiderWPF;component/Resources/Images/NightRider-02.png"));
+    }
+}
+// Checked by James Williams
