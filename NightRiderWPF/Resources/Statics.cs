e8d64b167c0a1410c72d7e533c036eeac9fe06e9
Nathan Toothaker <nathan-toothaker@student.kirkwood.edu>
2024-02-26 15:57:42 -0600

    added some page colors

--- NightRiderWPF/Resources/Statics.cs
+++ NightRiderWPF/Resources/Statics.cs
@@ -12,10 +12,13 @@ namespace NightRiderWPF.Resources
     {
         // Main colors
         public static SolidColorBrush BackgroundMain => new SolidColorBrush(Colors.AliceBlue);
+        public static SolidColorBrush BackgroundPage => new SolidColorBrush((Color)ColorConverter.ConvertFromString("#99999999"));
+        public static SolidColorBrush BackgroundItem => new SolidColorBrush((Color)ColorConverter.ConvertFromString("#EDE9E3"));
         public static SolidColorBrush PrimaryColor => new SolidColorBrush((Color)ColorConverter.ConvertFromString("#7081A3"));
         public static SolidColorBrush SecondaryColor => new SolidColorBrush((Color)ColorConverter.ConvertFromString("#3D527A"));
         public static SolidColorBrush AccentColor => new SolidColorBrush((Color)ColorConverter.ConvertFromString("#20355C"));
         public static SolidColorBrush ButtonText => new SolidColorBrush((Color)ColorConverter.ConvertFromString("#CFD5E2"));
+        public static SolidColorBrush StaticGrey => new SolidColorBrush((Color)ColorConverter.ConvertFromString("#999999"));
 
         // Images
         public static BitmapImage LogoWide => new BitmapImage(new Uri("pack://application:,,,/NightRiderWPF;component/Resources/Images/NightRider-01.png"));
